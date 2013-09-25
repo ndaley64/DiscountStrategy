b62219be4795191e4759a9c2f0121595bafa4fda
ndaley <ndaley@L11712038606.wctc.msft>
2013-09-25 18:30:17 -0500

    Changed QuantityDiscount to require a product quantity and discountAmount.

--- src/version1/QuantityDiscount.java
+++ src/version1/QuantityDiscount.java
@@ -5,10 +5,35 @@ package version1;
  * @author Nathaniel
  */
 public class QuantityDiscount implements Discount{
+    private int quantityLimit;
+    private double discountAmount;
+    
+    public QuantityDiscount(int quantityLimit, double discountAmount){
+        setQuantityLimit(quantityLimit);
+        setDiscountAmount(discountAmount);
+    }
+
+    public int getQuantityLimit() {
+        return quantityLimit;
+    }
+
+    public void setQuantityLimit(int quantityLimit) {
+        // need some validation
+        this.quantityLimit = quantityLimit;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(double discountAmount) {
+        // need some validation
+        this.discountAmount = discountAmount;
+    }
     
     @Override
     public double getDiscount(double numProducts) {
-        if(numProducts > 3){
+        if(numProducts >= 3){
             return .25;
         }else{
             return 0;
